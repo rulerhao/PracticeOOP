ea68c3fbece17929cf71c996c638f5ae5c7824c9
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-21 17:05:19 +0800

    1. Fixed a bug that steam engine should made by engineer with steam license.

--- lesson4/src/main/java/com/rulhouse/lesson4/main/Main.kt
+++ lesson4/src/main/java/com/rulhouse/lesson4/main/Main.kt
@@ -8,12 +8,8 @@ import com.rulhouse.lesson4.main.engineer.Engineer
 import com.rulhouse.lesson4.main.engineer.SteamEngineEngineer
 
 class Main {
-//    val engine = Engine(Engineer())   // It could be not runnable.
-//    val engineMadeBySteamEngineEngineer = Engine(SteamEngineEngineer())   // It could be not runnable.
-//    val steamEngine = SteamEngine(Engineer())  // It could be not runnable.
-//    val steamEngineMadeBySteamEngineEngineer = SteamEngine(SteamEngineEngineer())   // It could be not runnable.
     val computerScienceEngine = Engine(ComputerScienceEngineer())
     val computerScienceEngineMadeBySteamEngineer = Engine(ComputerScienceSteamEngineer())
-    val computerScienceSteamEngine = SteamEngine(ComputerScienceEngineer())
+//    val computerScienceSteamEngine = SteamEngine(ComputerScienceEngineer())   // It could be not runnable.
     val computerScienceSteamEngineMadeBySteamEngineer = SteamEngine(ComputerScienceSteamEngineer())
 }
--- lesson4/src/main/java/com/rulhouse/lesson4/main/engine/SteamEngine.kt
+++ lesson4/src/main/java/com/rulhouse/lesson4/main/engine/SteamEngine.kt
@@ -5,5 +5,5 @@ import com.rulhouse.lesson4.main.licence.ComputerScienceLicence
 import com.rulhouse.lesson4.main.licence.EngineeringLicence
 import com.rulhouse.lesson4.main.licence.SteamEngineLicence
 
-class SteamEngine<T>(engineer: T): Engine<T>(engineer) where T : Engineer, T : ComputerScienceLicence {
+class SteamEngine<T>(engineer: T): Engine<T>(engineer) where T : Engineer, T : ComputerScienceLicence, T : SteamEngineLicence {
 }
